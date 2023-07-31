f9114682e4627d35fe6b2c469fcfe2de67f22fb8
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-07-31 14:01:17 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+let welcomeMessage = "Welcome to Flatbook, "
+welcomeMessage = welcomeMessage + "Grace Hopper" + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+excitedWelcomeMessage = excitedWelcomeMessage + "GRACE HOPPER" + "!" 
+let shortGreeting = "Welcome, "
+shortGreeting = shortGreeting + "Grace" +"!"
